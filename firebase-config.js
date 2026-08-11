f6b254c4096938e7c7c750c9fb407f6dd2bb2c35
irdxo0102-spec <irdxo0102@gmail.com>
2026-08-11 15:22:37 +0600

    Add Firebase configuration

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// =====================================
+// IGS HOME CARE - FIREBASE CONFIG
+// =====================================
+
+const firebaseConfig = {
+    apiKey: "AIzaSyALZpVxWS7W7jFbFoKRsjRgBOT-xM6i7V0",
+    authDomain: "igs-home-care.firebaseapp.com",
+    projectId: "igs-home-care",
+    storageBucket: "igs-home-care.firebasestorage.app",
+    messagingSenderId: "388577708266",
+    appId: "1:388577708266:web:329166c6952b74d046af93",
+    measurementId: "G-58YGFY0H75"
+};
